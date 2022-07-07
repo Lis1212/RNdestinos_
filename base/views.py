a023bbd84ed372782d0a489c978fa384ed956075
myllene02 <myllene.souza@escolar.ifrn.edu.br>
2022-07-07 10:57:02 -0300

    deletei para organizar

--- base/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Olá, mundo! Esta é minha índice de enquetes")
-
-# Create your views here.
